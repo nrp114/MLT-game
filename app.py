ee6acc2cad1511ccab2a344c99df846030e1c782
Nisarg Patel <nrp114@scarletmail.rutgers.edu>
2023-03-31 18:47:38 -0700

    db config changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
 from waitress import serve
+import psycopg2
 
 # Create Flask application instance
 app = Flask(__name__, template_folder='templates')
@@ -11,7 +12,29 @@ app.config['SECRET_KEY'] = 'your-secret-key'
 socketio = SocketIO(app)
 
 # Define database schema
-# ...
+
+# Establish a connection to the database
+conn = psycopg2.connect(
+    host="mlt-game-public.cvkpaldfli6u.us-east-1.rds.amazonaws.com",
+    port=5432,
+    database="mlt-game-public",
+    user="postgress",
+    password="Password123",
+)
+
+# Create a cursor object
+cur = conn.cursor()
+
+# Execute a SQL query
+cur.execute("SELECT * FROM your_table_name")
+
+# Fetch the results
+results = cur.fetchall()
+
+# Close the cursor and connection
+cur.close()
+conn.close()
+
 
 # Define routes for creating and playing quizzes
 @app.route('/')
